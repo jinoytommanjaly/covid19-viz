d4bb3a1bff9ad688236056da2dd318cbcfc84002
Gnoeee <38123141+jinoytommanjaly@users.noreply.github.com>
2020-03-28 15:11:38 +0530

    Update dataViz.js

--- dataViz.js
+++ dataViz.js
@@ -331,15 +331,15 @@ legend.update = function() {
 function infobarUpdate() {
   var infobar = d3.select('#infobar');
   if (selected_date !== undefined) {
-    infobar.html('<b class="subtitle is-4" style="color:Crimson;">Active cases: ' +
+    infobar.html('<p class="subtitle is-4" style="color:Crimson;"><b>Active cases: ' +
       dataIndex.daily_bulletin[selected_date]['total_active'] +
-      '</b><br/> <b class="subtitle is-5" style="color:Teal;">Confirmed cases: ' +
+      '</b></p> <b class="subtitle is-5" style="color:Teal;">Confirmed cases: ' +
       dataIndex.daily_bulletin[selected_date]['total_positive'] +
-      '</b> <b class="subtitle is-5" style="color:Grey;">Deceased: ' +
+      '</b><br/> <b class="subtitle is-5" style="color:Grey;">Deceased: ' +
       dataIndex.daily_bulletin[selected_date]['deceased'] +
       '</b><br/> <b class="subtitle is-5" style="color:Tomato;">Samples taken: ' +
       dataIndex.daily_bulletin[selected_date]['sample_sent'] +
-      '</b> <b class="subtitle is-5" style="color:Green;">Samples negative: ' +
+      '</b><br/> <b class="subtitle is-5" style="color:Green;">Samples negative: ' +
       dataIndex.daily_bulletin[selected_date]['sample_negative'] +
       '</b>')
   }
